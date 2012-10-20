2f31da75e84a05aa002446e908597dcb694ba343
Nicolas Viennot <nicolas@viennot.biz>
2012-10-20 02:02:02 -0400

    Iterate on all the .class of a .jar

--- src/main/java/jd/core/Decompiler.java
+++ src/main/java/jd/core/Decompiler.java
@@ -1,14 +1,45 @@
 package jd.core;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.Map;
+import java.util.HashMap;
+import jd.ide.intellij.JavaDecompiler;
+
 public class Decompiler {
-    public static String decompile(String basePath, String internalClassName) throws DecompilerException {
-        String decompiled = new jd.ide.intellij.JavaDecompiler().decompile(basePath, internalClassName);
+    private JavaDecompiler decompiler;
+
+    public Decompiler() {
+        decompiler = new JavaDecompiler();
+    }
+
+    public String decompile(String jarPath, String internalClassName) throws DecompilerException {
+        String decompiled = decompiler.decompile(jarPath, internalClassName);
         if (!validContent(decompiled))
-            throw new DecompilerException("cannot decompile " + basePath + "!" +  internalClassName);
+            throw new DecompilerException("cannot decompile " + jarPath + "!" +  internalClassName);
         return decompiled;
     }
 
-    private static boolean validContent(String decompiled) {
+    public Map<String, String> decompile(String jarPath) throws DecompilerException, IOException {
+        ZipInputStream zip = new ZipInputStream(new FileInputStream(jarPath));
+        ZipEntry ze;
+        Map<String, String> classToJava = new HashMap<String, String>();
+
+        while ((ze = zip.getNextEntry()) != null ) {
+            String entryName = ze.getName();
+            if (entryName.endsWith(".class")) {
+                String className = entryName.replaceAll("\\$.*\\.class$", ".class");
+                if (!classToJava.containsKey(className))
+                    classToJava.put(className, decompiler.decompile(jarPath, className));
+            }
+        }
+
+        return classToJava;
+    }
+
+    private boolean validContent(String decompiled) {
         return decompiled != null && !decompiled.matches("(?sm)class\\s*\\{\\s*\\}.*");
     }
 }
--- src/main/java/jd/core/Main.java
+++ src/main/java/jd/core/Main.java
@@ -1,20 +1,29 @@
 package jd.core;
 
 import jd.core.Decompiler;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args.length < 2) {
-                System.out.println("Usage: jd <basePath> <internalClassName>");
+            if (args.length < 1) {
+                System.out.println("Usage: jd <jar> [internalClassName]");
                 return;
             }
-
             String basePath = args[0];
-            String internalClassName = args[1];
 
-            String result = Decompiler.decompile(basePath, internalClassName);
-            System.out.println(result);
+            if (args.length == 2) {
+                String internalClassName = args[1];
+                String result = new Decompiler().decompile(basePath, internalClassName);
+                System.out.println(result);
+            } else {
+                Map<String, String> classToJava = new Decompiler().decompile(basePath);
+                for (Map.Entry<String, String> entry : classToJava.entrySet()) {
+                    System.out.println("/* " + entry.getKey() + " */");
+                    System.out.println(entry.getValue());
+                    System.out.println("");
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
