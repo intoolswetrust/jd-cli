16f88ca016c48b1852f858745d811a87920f3a8b
Jacek <jacek@kuzemczak.co.uk>
2013-04-05 23:19:39 +0100

    Auto download the native library

--- src/main/java/jd/core/Main.java
+++ src/main/java/jd/core/Main.java
@@ -1,15 +1,25 @@
 package jd.core;
 
+import java.io.File;
+import java.net.URLDecoder;
+
 import jd.core.Decompiler;
+import jd.ide.intellij.JavaDecompiler;
 
 public class Main {
 	
+	public static String BASE_PATH;
+	
 	public static void main(String[] args){
 		try{
+			File jarFile = new File(URLDecoder.decode(JavaDecompiler.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8"));
+			
 			if (args.length < 2){
-				System.err.println("Usage: java -jar jd-core-java.jar <compiled.jar> <out_dir>");
+				System.err.println("Usage: java -jar " + jarFile.getName() + " <compiled.jar> <out_dir>");
 				return;
 			}
+			
+			Main.BASE_PATH = jarFile.getParent();
 			
 			String jarPath = args[0];
 			String outDirPath = args[1];
--- src/main/java/jd/ide/intellij/JavaDecompiler.java
+++ src/main/java/jd/ide/intellij/JavaDecompiler.java
@@ -1,20 +1,78 @@
 package jd.ide.intellij;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import jd.core.Main;
 
 public class JavaDecompiler {
 	
 	static{
-		String path = "";
+		String osName = System.getProperty("os.name").toLowerCase();
+		String os = "linux";
+		String arch = (System.getProperty("os.arch").contains("64")) ? "x86_64" : "x86";
+		String libEx = "so";
+		
+		if (osName.contains("win")){
+			os = "win32";
+			libEx = "dll";
+		}else if (osName.contains("mac")){
+			os = "macosx";
+			libEx = "jnilib";
+		}
+		
+		String libPath = Main.BASE_PATH + File.separator + "libjd-intellij." + libEx;
+		
+		File libFile = new File(libPath);
+		
+		if (!libFile.exists()){
+			System.out.println("Downloading native library");
+			
+			try{
+				URL url = new URL("https://bitbucket.org/bric3/jd-intellij/get/default.zip");
+				
+				URLConnection connection = url.openConnection();
+				connection.setReadTimeout(10000);
+				connection.setConnectTimeout(5000);
+				
+				BufferedInputStream input = new BufferedInputStream(connection.getInputStream());
+				ZipInputStream zip = new ZipInputStream(input);
+				
+				ZipEntry entry = null;
+				
+				while ((entry = zip.getNextEntry()) != null){
+					if (entry.getName().contains("native/nativelib/" + os + "/" + arch)){
+						FileOutputStream output = new FileOutputStream(libPath);
+						
+						byte[] buffer = new byte[4096];
+						int len;
+						
+						while ((len = zip.read(buffer)) != -1){
+							output.write(buffer, 0, len);
+						}
+						
+						output.close();
+						break;
+					}
+				}
+				
+				zip.close();
+				input.close();
+			}catch (Exception e){
+				System.err.println("Failed to download library");
+				e.printStackTrace();
+			}
+		}
 		
 		try{
-			path = JavaDecompiler.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-			path = java.net.URLDecoder.decode(path, "UTF-8");
-			path = (new File(path)).getParent();
-			
-			System.load(path + "/libjd-intellij.so");
+			System.load(libPath);
 		}catch (Exception e){
-			throw new IllegalStateException("Something got wrong when loading the Java Decompiler native lib at " + path);
+			e.printStackTrace();
 		}
 	}
 	
