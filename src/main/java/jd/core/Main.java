51fff3236a5a1152f84b03857e29c01537c77766
Josef Cacek <josef.cacek@gmail.com>
2013-09-16 17:03:17 +0200

    Typo fixed (and some TODO comments added)

--- src/main/java/jd/core/Main.java
+++ src/main/java/jd/core/Main.java
@@ -5,9 +5,15 @@ public class Main {
     public static void main(String[] args) {
         try {
             if (args.length == 0 || args.length > 2) {
-                System.err.println("Usage: java -jar jd-cmd <input.jar> [output]");
+                System.err.println("Usage: java -jar jd-cmd.jar <input.jar> [output]");
                 return;
             }
+
+            // TODO Decompiler.getInstance(), Decompiler.setOptions(DecompilerOptions) - test if the native implementation is
+            // thread safe.
+            // Single class decompilation - with possible (default?) output to console
+            // Decompile to zip (configurable in DecompilerOptions?)
+            // instead of the decompileToDir use sth. like decompile
 
             final String outName = args.length == 1 ? args[0] + ".src" : args[1];
             (new Decompiler()).decompileToDir(args[0], outName);
